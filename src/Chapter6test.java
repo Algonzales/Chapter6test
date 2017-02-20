c9534180a6b91e8564085090a145a808404b336e
Algonzales <gonzo2356@gmail.com>
2017-02-20 13:51:23 -0600

    change 2
    I finalized a lot of changes so I can put it in the non test build

--- src/Chapter6test.java
+++ src/Chapter6test.java
@@ -61,197 +61,19 @@ public class Chapter6test extends JFrame {
 
 		subtractTutorMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				long lStartTime = System.nanoTime();
-				int correctAnswer = 0;
-				int questionNumber = 1;
-				JTextField userAnswerField = new JTextField(5);
-				JPanel myPanel = new JPanel();
-				while (correctAnswer < 4) {
-					int num1 = (int) (Math.random() * 10 + 1);
-					int num2 = (int) (Math.random() * 10 + 1);
-					int answer = 0;
-					myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-					myPanel.add(new JLabel("Question " + questionNumber));
-					if (num1 > num2) {
-						answer = num1 - num2;
-						myPanel.add(new JLabel("Get 4 questions correct to exit"));
-						myPanel.add(new JLabel("Questions correct so far " + correctAnswer));
-						myPanel.add(new JLabel("What is " + num1 + " - " + num2 + "?"));
-					} else if (num2 > num1) {
-						answer = num2 - num1;
-						myPanel.add(new JLabel("Get 4 questions correct to exit"));
-						myPanel.add(new JLabel("Questions correct so far " + correctAnswer));
-						myPanel.add(new JLabel("What is " + num2 + " - " + num1 + "?"));
-					} else {
-						answer = num1 - num2;
-						myPanel.add(new JLabel("Get 4 questions correct to exit"));
-						myPanel.add(new JLabel("Questions correct so far " + correctAnswer));
-						myPanel.add(new JLabel("What is " + num1 + " - " + num2 + "?"));
-					}
-					myPanel.add(new JLabel("Enter your answer:"));
-					myPanel.add(userAnswerField);
-					int result = JOptionPane.showConfirmDialog(null, myPanel, "Subtraction tutor",
-							JOptionPane.OK_CANCEL_OPTION);
-					int userAnswer = Integer.parseInt(userAnswerField.getText());
-
-					if (result == JOptionPane.CANCEL_OPTION) {
-						correctAnswer = 5;
-
-					} else if (result == JOptionPane.OK_OPTION) {
-						if (userAnswer == answer) {
-							JOptionPane.showMessageDialog(null, "Correct, the answer is  " + answer, // message
-									"Answer for number " + questionNumber, // title
-									JOptionPane.PLAIN_MESSAGE);
-							correctAnswer += 1;
-							questionNumber += 1;
-							// remove all components in panel.
-							myPanel.removeAll();
-							// refresh the panel.
-							myPanel.updateUI();
-
-						} else if (userAnswer != answer) {
-							JOptionPane.showMessageDialog(null, "Incorrect, the correct answer is  " + answer, // message
-									"Answer for number " + questionNumber, // title
-									JOptionPane.PLAIN_MESSAGE);
-							questionNumber += 1;
-							// remove all components in panel.
-							myPanel.removeAll();
-							// refresh the panel.
-							myPanel.updateUI();
-
-						} else {
-							correctAnswer = 5;
-						}
-
-					}
-				}
-				long lEndTime = System.nanoTime();
-				long output = (lEndTime - lStartTime) / 1000000000;
-				JOptionPane.showMessageDialog(null, "Time taken " + output + " Seconds", // message
-						"Time taken", // title
-						JOptionPane.PLAIN_MESSAGE);
+				displayTutor("Subtract");
 			}
 
 		});
 		multiplyTutorMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				int correctAnswer = 0;
-				int questionNumber = 1;
-				long lStartTime = System.nanoTime();
-				JTextField userAnswerField = new JTextField(5);
-
-				JPanel myPanel = new JPanel();
-				while (correctAnswer < 4) {
-					int num1 = (int) (Math.random() * 10 + 1);
-					int num2 = (int) (Math.random() * 10 + 1);
-					myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-					myPanel.add(new JLabel("Question " + questionNumber));
-					myPanel.add(new JLabel("Get 4 questions correct to exit"));
-					myPanel.add(new JLabel("Questions correct so far " + correctAnswer));
-					myPanel.add(new JLabel("What is " + num1 + " * " + num2 + "?"));
-					myPanel.add(new JLabel("Enter your answer:"));
-					myPanel.add(userAnswerField);
-					int result = JOptionPane.showConfirmDialog(null, myPanel, "Multiplication tutor",
-							JOptionPane.OK_CANCEL_OPTION);
-					int userAnswer = Integer.parseInt(userAnswerField.getText());
-					int answer = num1 * num2;
-					if (result == JOptionPane.CANCEL_OPTION) {
-						correctAnswer = 5;
-
-					} else if (result == JOptionPane.OK_OPTION) {
-						if (userAnswer == answer) {
-							JOptionPane.showMessageDialog(null, "Correct, the answer is  " + answer, // message
-									"Answer for number " + questionNumber, // title
-									JOptionPane.PLAIN_MESSAGE);
-							correctAnswer += 1;
-							questionNumber += 1;
-							// remove all components in panel.
-							myPanel.removeAll();
-							// refresh the panel.
-							myPanel.updateUI();
-
-						} else if (userAnswer != answer) {
-							JOptionPane.showMessageDialog(null, "Incorrect, the correct answer is  " + answer, // message
-									"Answer for number " + questionNumber, // title
-									JOptionPane.PLAIN_MESSAGE);
-							questionNumber += 1;
-							// remove all components in panel.
-							myPanel.removeAll();
-							// refresh the panel.
-							myPanel.updateUI();
-
-						} else {
-							correctAnswer = 5;
-						}
-
-					}
-				}
-				long lEndTime = System.nanoTime();
-				long output = (lEndTime - lStartTime) / 1000000000;
-				JOptionPane.showMessageDialog(null, "Time taken " + output + " Seconds", // message
-						"Time taken", // title
-						JOptionPane.PLAIN_MESSAGE);
+				displayTutor("Multiply");
 			}
 
 		});
 		divideTutorMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				long lStartTime = System.nanoTime();
-				int correctAnswer = 0;
-				int questionNumber = 1;
-				JTextField userAnswerField = new JTextField(5);
-
-				JPanel myPanel = new JPanel();
-				while (correctAnswer < 4) {
-					int num1 = (int) (Math.random() * 10) + 1;
-					int num2 = (int) (Math.random() * 10) + 1;
-					int num3 = num1 * num2;
-					int answer = num3 / num1;
-					myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-					myPanel.add(new JLabel("Get 4 questions correct to exit or press cancel"));
-					myPanel.add(new JLabel("Questions correct so far " + correctAnswer));
-					myPanel.add(new JLabel("What is " + num3 + " / " + num1 + "?"));
-					myPanel.add(new JLabel("Enter your answer:"));
-					myPanel.add(userAnswerField);
-					int result = JOptionPane.showConfirmDialog(null, myPanel, "Division tutor",
-							JOptionPane.OK_CANCEL_OPTION);
-					int userAnswer = Integer.parseInt(userAnswerField.getText());
-					if (result == JOptionPane.CANCEL_OPTION) {
-						correctAnswer = 5;
-
-					} else if (result == JOptionPane.OK_OPTION) {
-						if (userAnswer == answer) {
-							JOptionPane.showMessageDialog(null, "Correct, the answer is  " + answer, // message
-									"Answer for number " + questionNumber, // title
-									JOptionPane.PLAIN_MESSAGE);
-							correctAnswer += 1;
-							questionNumber += 1;
-							// remove all components in panel.
-							myPanel.removeAll();
-							// refresh the panel.
-							myPanel.updateUI();
-
-						} else if (userAnswer != answer) {
-							JOptionPane.showMessageDialog(null, "Incorrect, the correct answer is  " + answer, // message
-									"Answer for number " + questionNumber, // title
-									JOptionPane.PLAIN_MESSAGE);
-							questionNumber += 1;
-							// remove all components in panel.
-							myPanel.removeAll();
-							// refresh the panel.
-							myPanel.updateUI();
-
-						} else {
-							correctAnswer = 5;
-						}
-
-					}
-				}
-				long lEndTime = System.nanoTime();
-				long output = (lEndTime - lStartTime) / 1000000000;
-				JOptionPane.showMessageDialog(null, "Time taken " + output + " Seconds", // message
-						"Time taken", // title
-						JOptionPane.PLAIN_MESSAGE);
+				displayTutor("Divide");
 			}
 
 		});
@@ -262,8 +84,8 @@ public class Chapter6test extends JFrame {
 			}
 		});
 	}
-	
-	public static void displayTutor(String tutorType){
+
+	public static void displayTutor(String tutorType) {
 		long lStartTime = System.nanoTime();
 		int correctAnswer = 0;
 		int questionNumber = 1;
@@ -273,17 +95,57 @@ public class Chapter6test extends JFrame {
 		while (correctAnswer < 4) {
 			int num1 = (int) (Math.random() * 10 + 1);
 			int num2 = (int) (Math.random() * 10 + 1);
+			int num3 = num1 * num2;
+			int answer = 0;
 			myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
 			myPanel.add(new JLabel("Question " + questionNumber));
 			myPanel.add(new JLabel("Get 4 questions correct to exit"));
 			myPanel.add(new JLabel("Questions correct so far " + correctAnswer));
-			myPanel.add(new JLabel("What is " + num1 + " + " + num2 + "?"));
+
+			switch (tutorType) {
+			case "Add":
+				myPanel.add(new JLabel("What is " + num1 + " + " + num2 + "?"));
+				break;
+			case "Subtract":
+				if (num1 > num2) {
+					myPanel.add(new JLabel("What is " + num1 + " - " + num2 + "?"));
+				}
+				if (num2 > num1) {
+					myPanel.add(new JLabel("What is " + num2 + " - " + num1 + "?"));
+				}
+				break;
+			case "Multiply":
+				myPanel.add(new JLabel("What is " + num1 + " * " + num2 + "?"));
+				break;
+			case "Divide":
+				myPanel.add(new JLabel("What is " + num3 + " / " + num1 + "?"));
+				break;
+			}
+
 			myPanel.add(new JLabel("Enter your answer:"));
 			myPanel.add(userAnswerField);
-			int result = JOptionPane.showConfirmDialog(null, myPanel, "Addition tutor",
-					JOptionPane.OK_CANCEL_OPTION);
+
+			switch (tutorType) {
+			case "Add":
+				answer = num1 + num2;
+				break;
+			case "Subtract":
+				if (num1 > num2) {
+					answer = num1 - num2;
+				}
+				if (num2 > num1) {
+					answer = num2 - num1;
+				}
+				break;
+			case "Multiply":
+				answer = num1 * num2;
+				break;
+			case "Divide":
+				answer = num3 / num1;
+				break;
+			}
+			int result = JOptionPane.showConfirmDialog(null, myPanel, "Addition tutor", JOptionPane.OK_CANCEL_OPTION);
 			int userAnswer = Integer.parseInt(userAnswerField.getText());
-			int answer = num1 + num2;
 			if (result == JOptionPane.CANCEL_OPTION) {
 				correctAnswer = 5;
 
@@ -319,7 +181,7 @@ public class Chapter6test extends JFrame {
 		JOptionPane.showMessageDialog(null, "Time taken " + output + " Seconds", // message
 				"Time taken", // title
 				JOptionPane.PLAIN_MESSAGE);
-		
+
 	}
 
 	public static void main(String[] args) {
